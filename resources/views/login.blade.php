bd800070c2d684a84ffff4d1624a3198a64c005d
Ankit Singh <ankit.singh@codeclouds.in>
2021-03-14 12:10:57 +0530

    new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/ProductController.php 	new file:   app/Http/Controllers/UserController.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/UserAuth.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2021_03_13_154133_create_users_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   database/seeders/UserSeeder.php 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   public/web.config 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/lang/en/auth.php 	new file:   resources/lang/en/pagination.php 	new file:   resources/lang/en/passwords.php 	new file:   resources/lang/en/validation.php 	new file:   resources/views/footer.blade.php 	new file:   resources/views/header.blade.php 	new file:   resources/views/login.blade.php 	new file:   resources/views/master.blade.php 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   server.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   webpack.mix.js

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+        <form action="login" method="POST">
+            <div class="form-group">
+             @csrf
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-default">Login</button>
+        </form>
+        </div>
+    </div>
+</div>
+@endsection
